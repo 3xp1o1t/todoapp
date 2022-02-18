fb84d267134d93c911064aef49f4d4ec0f0baf94
3xp1o1t <jes.monti.503@gmail.com>
2022-02-17 22:18:06 -0600

    Se agrego crear una tarea todo.
    Se modifico TodoList para que no solo liste los todos, sino que tambien permita la creacion de nuevos.
    Se agregaron las urls del api al proyecto backend urls.py.
    Se agrego una url unica para listar y poder crear todos.

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import TodoSerializer
+from todo.models import Todo
 
-# Create your views here.
+class TodoListCreate(generics.ListCreateAPIView):
+    # Heredar de ListAPIView requiere 2 atributos obligatorios. 
+    # El primero: SerializerClass 
+    # El segundo: QuerySet.
+
+    serializer_class = TodoSerializer
+
+    def get_queryset(self):
+        user = self.request.user 
+
+        return Todo.objects.filter(user=user).order_by('-created')
+
+    # Para poder agregar un usuario automaticamente al crear una tarea. 
+    # Funciona como un Hook, el cual es llamado antes de que se cree el registro en la DB.
+    # Esto debido a que se requiere al momento de crear una tarea por el ForeignKey. 
+    def perform_create(self, serializer):
+        # serializer mantiene el modelo
+        serializer.save(user=self.request.user) 
